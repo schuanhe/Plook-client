3d09a76498d43adaa8c1671cb6fce699cf705bd0
schuanhe <schuanhe@qq.com>
2024-05-20 15:16:44 +0800

    Changes

---
+++ src/components/PlookVideo.vue
@@ -0,0 +1,72 @@
+<template>
+  <view class="plook-video">
+    <video id="myVideo" src="https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/2minute-demo.mp4">
+    </video>
+  </view>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+const src = ref('https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/2minute-demo.mp4');
+const danmuList = ref([
+  {
+    text: '第 1s 出现的弹幕',
+    color: '#ff0000',
+    time: 1
+  },
+  {
+    text: '第 3s 出现的弹幕',
+    color: '#ff00ff',
+    time: 3
+  }
+]);
+const danmuValue = ref('');
+let videoContext = null;
+
+const sendDanmu = () => {
+  if (videoContext) {
+    videoContext.sendDanmu({
+      text: danmuValue.value,
+      color: getRandomColor()
+    });
+    danmuValue.value = '';
+  }
+};
+
+const videoErrorCallback = (e) => {
+  uni.showModal({
+    content: e.target.errMsg,
+    showCancel: false
+  });
+};
+
+const getRandomColor = () => {
+  const rgb = [];
+  for (let i = 0; i < 3; ++i) {
+    let color = Math.floor(Math.random() * 256).toString(16);
+    color = color.length === 1 ? '0' + color : color;
+    rgb.push(color);
+  }
+  return '#' + rgb.join('');
+};
+
+onMounted(() => {
+  // #ifndef MP-ALIPAY
+  videoContext = uni.createVideoContext('myVideo');
+  // #endif
+});
+
+</script>
+
+<style scoped>
+.plook-video {
+  width: 100%;
+  height: 100%;
+}
+
+.plook-video video {
+  width: 100%;
+  height: 100%;
+  object-fit: contain; /* 根据需求调整，如cover, contain等 */
+}
+</style>
---
+++ src/pages/PlookRoom/PlookRoom.vue
@@ -0,0 +1,63 @@
+<template>
+  <view id="app" class="container">
+    <view class="video-container">
+      <PlookVideo />
+    </view>
+    <view class="comments-container">
+      <h2>评论区</h2>
+      <p>这是一个评论。</p>
+      <!-- 更多评论 -->
+    </view>
+  </view>
+</template>
+
+<script setup lang="ts">
+
+import PlookVideo from "../../components/PlookVideo.vue";
+</script>
+
+
+<style scoped>
+body, html {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: Arial, sans-serif;
+}
+
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 10px;
+}
+
+.video-container {
+  width: 100vw;
+  height: calc(100vw * 9 / 16);
+  background: #4cd964;
+}
+
+.comments-container {
+  width: 100%;
+  margin-top: 10px;
+  background: #007aff;
+}
+
+/* 对于宽度大于768px的屏幕，使用横向布局 */
+@media (min-width: 768px) {
+  .container {
+    flex-direction: row;
+    justify-content: space-between;
+  }
+
+  .video-container {
+    width: 60vw;
+    height: calc(60vw * 9 / 16);
+  }
+
+  .comments-container {
+    width: 35%;
+  }
+}
+</style>
