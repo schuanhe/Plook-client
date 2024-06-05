4adaae448fe6e8a15fa4113903dab050fed9addf
schuanhe <schuanhe@qq.com>
2024-06-05 17:37:27 +0800

    * 初始化数据库分支

--- server/src/utils/db.ts
+++ server/src/utils/db.ts
@@ -8,13 +8,14 @@ let sequelize: Sequelize;
 
 function initializeSequelize(): Sequelize {
     if (config.database.dialect === 'mysql') {
-        sequelize = new Sequelize(    config.database.mysql.database, // 数据库名
+        sequelize = new Sequelize(
+            config.database.mysql.database, // 数据库名
             config.database.mysql.username, // 用户名
             config.database.mysql.password, // 密码
             {
                 host: config.database.mysql.host, // 主机地址
                 port: config.database.mysql.port, // 端口号
-                dialect: config.database.mysql.dialect as 'mysql', // 明确指定方言为 'mysql'
+                dialect: 'mysql', // 明确指定方言为 'mysql'
             });
     } else if (config.database.dialect === 'sqlite3') {
         const sqlitePath = path.join(config.database.sqlite3.storage);
