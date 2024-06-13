d7a56808438fdedd50b533925a004705987664ed
schuanhe <schuanhe@qq.com>
2024-06-13 17:43:42 +0800

    重构（refactor）socketHandler：改进房间管理和消息处理逻辑 目的：优化socketHandler服务，以利用新创建的SocketService类来管理套接字连接、房间互动及消息广播功能。 改动内容： 采用SocketService：将原本直接在socketHandler中处理的关于套接字连接、房间管理及消息传播的任务，转交给SocketService处理。这一调整使得事件处理逻辑更加简洁清晰。 简化事件处理：通过委托给SocketService，减少了socketHandler中的直接逻辑实现，提高了代码的可维护性和可读性。 引入类型注解：为CustomSocket和SocketMessage引入类型定义，增强了代码的类型安全性和可靠性，有助于开发阶段发现潜在类型错误。 效果：此番重构不仅提升了代码结构的合理性，还通过类型系统的加强，降低了因类型不匹配导致的错误风险，整体上提高了系统的稳定性和开发效率。

---
+++ server/src/services/impl/socketService.ts
@@ -0,0 +1,97 @@
+
+import type {Server} from "socket.io";
+import {CustomSocket, SocketEvent, SocketMessage} from "../socketHandler";
+import { roomService } from "./roomService";
+
+
+export class SocketService {
+    private socket:CustomSocket
+
+    constructor(socket: CustomSocket) {
+        this.socket = socket;
+    }
+
+    // ROOM_INFO 事件处理
+
+    public joinRoom(message: SocketMessage) {
+        const {roomId} = message.data;
+        if (!roomId){
+            console.log('房间号不能为空');
+            this.socket.emit(SocketEvent.ERROR, '房间号不能为空');
+            return;
+        }
+        this.socket.join(String(roomId));
+        console.log(`${this.socket.id} 加入房间 ${roomId}`);
+    }
+
+    public leaveRoom(message: SocketMessage) {
+        const {roomId} = message.data;
+        if (!roomId){
+            console.log('房间号不能为空');
+            this.socket.emit(SocketEvent.ERROR, '房间号不能为空');
+            return;
+        }
+        this.socket.leave(String(roomId));
+        console.log(`${this.socket.id} 离开房间 ${roomId}`);
+    }
+
+    public getRoomInfo(message: SocketMessage, io: Server) {
+        const { roomId } = message.data;
+        if (!roomId) {
+            console.log('房间号不能为空');
+            this.socket.emit(SocketEvent.ERROR, '房间号不能为空');
+            return;
+        }
+
+        const roomSockets = io.sockets.adapter.rooms.get(String(roomId));
+        if (roomSockets) {
+            const clients = Array.from(roomSockets).map(socketId => {
+                const clientSocket = io.sockets.sockets.get(socketId) as CustomSocket;
+                return { userId: clientSocket?.user };
+            });
+            this.socket.emit(SocketEvent.ROOM_INFO, clients);
+        } else {
+            console.log(`房间 ${roomId} 不存在`);
+            this.socket.emit(SocketEvent.ERROR, `房间 ${roomId} 不存在`);
+        }
+    }
+
+    sendMessage(socketMessage: SocketMessage, io: Server) {
+        const { roomId, message } = socketMessage.data;
+        if (!roomId || !message) {
+            console.log('房间号或消息不能为空');
+            this.socket.emit(SocketEvent.ERROR, '房间号或消息不能为空');
+            return;
+        }
+
+        if (this.socket.rooms.has(String(roomId))) {
+            io.to(String(roomId)).emit(SocketEvent.ROOM_MESSAGE, {
+                userId: this.socket.user,
+                message: message
+            });
+        } else {
+            console.log(`${this.socket.id} 未加入房间 ${roomId}`);
+            this.socket.emit(SocketEvent.ERROR, `${this.socket.id} 未加入房间 ${roomId}`);
+        }
+
+    }
+    getRoomDbInfo(socketMessage: SocketMessage) {
+        const { roomId } = socketMessage.data;
+        const userId = this.socket.user;
+        if (!roomId || !userId || !Number(userId) || !Number(roomId)) {
+            this.socket.emit(SocketEvent.ERROR, '参数错误');
+            return;
+        }
+        roomService.getRoomInfo(roomId).then((roomInfo) => {
+            if (roomInfo) {
+                this.socket.emit(SocketEvent.ROOM_DB_INFO, roomInfo);
+            } else {
+                this.socket.emit(SocketEvent.ROOM_DB_INFO, '房间不存在');
+            }
+        }).catch(() => {
+            this.socket.emit(SocketEvent.ROOM_DB_INFO, '获取房间信息失败');
+        })
+
+
+    }
+}
